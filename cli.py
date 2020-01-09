003bd7734769cd07e51fab882cdebbb7eba12715
Kevin O'Brien <kpobrien@buffalo.edu>
2020-01-09 17:38:40 -0500

    working on adding to spent column

--- cli.py
+++ cli.py
@@ -1,14 +1,69 @@
 import click
+from openpyxl import Workbook
+import os
+import datetime
 
+wb = Workbook()
+ws = wb.active
+ws.title = "Expense Tracker"
+
+# Row Titles
+ws['A1'] = 'Variable Expenses'
+ws['A2'] = 'Rent'
+ws['A3'] = 'Gas'
+ws['A4'] = 'Groceries'
+ws['A5'] = 'Restaurants'
+ws['A6'] = 'Student Loan'
+ws['A6'] = 'Savings'
+ws['A7'] = 'Recreation'
+
+# Formatting
+ws.column_dimensions['A'].width = 20
+    
+# Dates
+ws['B2'] = datetime.datetime(2019, 1, 9).strftime("%m/%d/%y")
+
+# Column Titles
+ws['B1'] = 'Date'
+ws['C1'] = 'Budgeted'
+ws['D1'] = 'Spent'
+ws['E1'] = 'Remaining'
+
+for row in range(2, 8):
+     for col in range(2, 5):
+        _ = ws.cell(column=col, row=row, value="{0}".format("0"))
+
+
+for row in range(2, 8):
+     for col in range(5, 6):
+        _ = ws.cell(column=col, row=row, value="{0}".format("=(C2:C7 - D2:D7)"))
+
+# Budgeted amounts
+ws['C2'] = 900
+ws['C3'] = 200
+ws['C4'] = 200
+
+# Spent amounts
+
+wb.save("expense_tracker.xlsx")
+
+os.system("python3 spreadsheet.py")
 
         
 @click.command()
-@click.option('--category', prompt='In category of', help='Category money was spent in.')
+@click.option('--category', prompt='Category', help='Category money was spent in.')
 @click.option('--expense', prompt='You have spent', default=0.00, help='Amount spent.')
 def spent(expense, category):
-    """Simple program that tracks EXPENSES in CATEGORIES."""
+    """Simple program that tracks EXPENSES in various CATEGORIES."""
     click.echo('Spent ${:,.2f} on %s'.format(expense) % ( category))
+    addSpent(category, expense)
+
+def addSpent(cat, amt):
+    ws['D3'] = float(ws['D3'].value) + float(amt)
+    wb.save("expense_tracker.xlsx")
 
 if __name__ == '__main__':
     spent()
+
+
 
--- spreadsheet.py
+++ spreadsheet.py
@@ -1,49 +1,48 @@
-from openpyxl import Workbook
-from openpyxl.utils import get_column_letter
-import datetime
+# from openpyxl import Workbook
+# import datetime
 
-wb = Workbook()
-ws = wb.active
-ws.title = "Expense Tracker"
+# wb = Workbook()
+# ws = wb.active
+# ws.title = "Expense Tracker"
 
-# Row Titles
-ws['A1'] = 'Variable Expenses'
-ws['A2'] = 'Rent'
-ws['A3'] = 'Gas'
-ws['A4'] = 'Groceries'
-ws['A5'] = 'Restaurants'
-ws['A6'] = 'Student Loan'
-ws['A6'] = 'Savings'
-ws['A7'] = 'Recreation'
+# # Row Titles
+# ws['A1'] = 'Variable Expenses'
+# ws['A2'] = 'Rent'
+# ws['A3'] = 'Gas'
+# ws['A4'] = 'Groceries'
+# ws['A5'] = 'Restaurants'
+# ws['A6'] = 'Student Loan'
+# ws['A6'] = 'Savings'
+# ws['A7'] = 'Recreation'
 
-# Formatting
-ws.column_dimensions['A'].width = 20
+# # Formatting
+# ws.column_dimensions['A'].width = 20
 
-for row in range(2, 8):
-     for col in range(3, 6):
-        _ = ws.cell(column=col, row=row, value="{0}".format(0))
+# for row in range(2, 8):
+#      for col in range(3, 6):
+#         _ = ws.cell(column=col, row=row, value="{0}".format(0))
     
-# Dates
-ws['B2'] = datetime.datetime(2019, 1, 9).strftime("%m/%d/%y")
+# # Dates
+# ws['B2'] = datetime.datetime(2019, 1, 9).strftime("%m/%d/%y")
 
-# Column Titles
-ws['B1'] = 'Date'
-ws['C1'] = 'Budgeted'
-ws['D1'] = 'Spent'
-ws['E1'] = 'Remaining'
-
-
-for row in range(2, 8):
-     for col in range(5, 6):
-        _ = ws.cell(column=col, row=row, value="{0}".format("=(C2:C7 - D2:D7)"))
-
-# Budgeted amounts
-ws['C2'] = 900
-ws['C3'] = 200
-ws['C4'] = 200
-
-# Spent amounts
-ws['D3'] = 168.71
+# # Column Titles
+# ws['B1'] = 'Date'
+# ws['C1'] = 'Budgeted'
+# ws['D1'] = 'Spent'
+# ws['E1'] = 'Remaining'
 
 
-wb.save("expense_tracker.xlsx")
+# for row in range(2, 8):
+#      for col in range(5, 6):
+#         _ = ws.cell(column=col, row=row, value="{0}".format("=(C2:C7 - D2:D7)"))
+
+# # Budgeted amounts
+# ws['C2'] = 900
+# ws['C3'] = 200
+# ws['C4'] = 200
+
+# # Spent amounts
+# ws['D3'] = 168.71
+
+
+# wb.save("expense_tracker.xlsx")
