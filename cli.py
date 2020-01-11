079f7b1ec6e8e7066224cbde19ab68b3047bcf36
Kevin O'Brien <kpobrien@buffalo.edu>
2020-01-11 16:43:07 -0500

    functioning addition of values

--- cli.py
+++ cli.py
@@ -1,51 +1,11 @@
 import click
-from openpyxl import Workbook
+from openpyxl import Workbook, load_workbook
 import os
 import datetime
 
-wb = Workbook()
+
+wb = load_workbook(filename = "expense_tracker.xlsx")
 ws = wb.active
-ws.title = "Expense Tracker"
-
-# Row Titles
-ws['A1'] = 'Variable Expenses'
-ws['A2'] = 'Rent'
-ws['A3'] = 'Gas'
-ws['A4'] = 'Groceries'
-ws['A5'] = 'Restaurants'
-ws['A6'] = 'Student Loan'
-ws['A6'] = 'Savings'
-ws['A7'] = 'Recreation'
-
-# Formatting
-ws.column_dimensions['A'].width = 20
-    
-# Dates
-ws['B2'] = datetime.datetime(2019, 1, 9).strftime("%m/%d/%y")
-
-# Column Titles
-ws['B1'] = 'Date'
-ws['C1'] = 'Budgeted'
-ws['D1'] = 'Spent'
-ws['E1'] = 'Remaining'
-
-for row in range(2, 8):
-     for col in range(2, 5):
-        _ = ws.cell(column=col, row=row, value="{0}".format("0"))
-
-
-for row in range(2, 8):
-     for col in range(5, 6):
-        _ = ws.cell(column=col, row=row, value="{0}".format("=(C2:C7 - D2:D7)"))
-
-# Budgeted amounts
-ws['C2'] = 900
-ws['C3'] = 200
-ws['C4'] = 200
-
-# Spent amounts
-
-wb.save("expense_tracker.xlsx")
 
 os.system("python3 spreadsheet.py")
 
--- spreadsheet.py
+++ spreadsheet.py
@@ -1,48 +1,49 @@
-# from openpyxl import Workbook
-# import datetime
+from openpyxl import Workbook
+import datetime
 
-# wb = Workbook()
-# ws = wb.active
-# ws.title = "Expense Tracker"
+wb = Workbook()
+ws = wb.active
+ws.title = "Expense Tracker"
 
-# # Row Titles
-# ws['A1'] = 'Variable Expenses'
-# ws['A2'] = 'Rent'
-# ws['A3'] = 'Gas'
-# ws['A4'] = 'Groceries'
-# ws['A5'] = 'Restaurants'
-# ws['A6'] = 'Student Loan'
-# ws['A6'] = 'Savings'
-# ws['A7'] = 'Recreation'
+# Row Titles
+ws['A1'] = 'Variable Expenses'
+ws['A2'] = 'Rent'
+ws['A3'] = 'Gas'
+ws['A4'] = 'Groceries'
+ws['A5'] = 'Restaurants'
+ws['A6'] = 'Student Loan'
+ws['A6'] = 'Savings'
+ws['A7'] = 'Recreation'
 
-# # Formatting
-# ws.column_dimensions['A'].width = 20
-
-# for row in range(2, 8):
-#      for col in range(3, 6):
-#         _ = ws.cell(column=col, row=row, value="{0}".format(0))
+# Formatting
+ws.column_dimensions['A'].width = 20
     
-# # Dates
-# ws['B2'] = datetime.datetime(2019, 1, 9).strftime("%m/%d/%y")
+# Dates
+ws['B2'] = datetime.datetime(2019, 1, 9).strftime("%m/%d/%y")
 
-# # Column Titles
-# ws['B1'] = 'Date'
-# ws['C1'] = 'Budgeted'
-# ws['D1'] = 'Spent'
-# ws['E1'] = 'Remaining'
+# Column Titles
+ws['B1'] = 'Date'
+ws['C1'] = 'Budgeted'
+ws['D1'] = 'Spent'
+ws['E1'] = 'Remaining'
 
-
-# for row in range(2, 8):
-#      for col in range(5, 6):
-#         _ = ws.cell(column=col, row=row, value="{0}".format("=(C2:C7 - D2:D7)"))
-
-# # Budgeted amounts
-# ws['C2'] = 900
-# ws['C3'] = 200
-# ws['C4'] = 200
-
-# # Spent amounts
-# ws['D3'] = 168.71
+for row in range(2, 8):
+     for col in range(2, 5):
+        _ = ws.cell(column=col, row=row, value="{0}".format("0"))
 
 
-# wb.save("expense_tracker.xlsx")
+for row in range(2, 8):
+     for col in range(5, 6):
+        _ = ws.cell(column=col, row=row, value="{0}".format("=(C2:C7 - D2:D7)"))
+
+# Budgeted amounts
+ws['C2'] = 900
+ws['C3'] = 200
+ws['C4'] = 200
+
+# Spent amounts
+
+# Spent amounts
+
+
+wb.save("expense_tracker.xlsx")
